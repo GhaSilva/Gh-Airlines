9b8f53b33272778efe9801c791dac123bfaf5de4
GhaSilva <ghabriel-silva@outlook.com>
2022-05-30 21:40:23 -0300

    models, migrations, seeds

---
+++ api/models/clientes.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Clientes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Clientes.init({
+    nome: DataTypes.STRING,
+    cpf: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Clientes',
+  });
+  return Clientes;
+};
---
+++ api/seeders/20220531002653-demo-cliente.js
@@ -0,0 +1,64 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    //insert into clientes(nome, cpf, createdAt, updatedAt) values ("Carla gomes", "48078246756", now(), now());
+    //insert into clientes(nome, cpf, createdAt, updatedAt) values ("Ghabriel Silva", "48078256753", now(), now());
+    //insert into clientes(nome, cpf, createdAt, updatedAt) values ("Rafaela Von Trapp", "48078246723", now(), now());
+    //insert into clientes(nome, cpf, createdAt, updatedAt) values ("George Ferreira", "48078246753", now(), now());
+    //insert into clientes(nome, cpf, createdAt, updatedAt) values ("Ryan Araújo", "38078246753", now(), now());
+    //insert into clientes(nome, cpf, createdAt, updatedAt) values ("Renato Lopes", "48578246753", now(), now());
+
+    await queryInterface.bulkInsert(
+      "Clientes",
+      [
+        {
+          nome: "Carla gomes",
+          cpf: "48078246756",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nome: "Ghabriel Silva",
+          cpf: "48078256753",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nome: "Rafaela Von Trapp",
+          cpf: "48078246723",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nome: "George Ferreira",
+          cpf: "48078246753",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nome: "Ryan Araújo",
+          cpf: "38078246753",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nome: "Renato Lopes",
+          cpf: "48578246753",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
