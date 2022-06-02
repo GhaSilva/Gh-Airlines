1a964e181450fb2429d76a7a190a9b20d820eced
GhaSilva <ghabriel-silva@outlook.com>
2022-06-02 15:55:23 -0300

    Projeto finalizado

--- api/models/passagem.js
+++ api/models/passagem.js
@@ -1,30 +1,25 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Passagems extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      Passagems.belongsTo(models.Voos, {foreignKey: 'voo_id'})
-      Passagems.belongsTo(models.Clientes, {foreignKey: 'comprador'})
+      Passagems.belongsTo(models.Voos, { foreignKey: "voo_id" });
+      Passagems.belongsTo(models.Clientes, { foreignKey: "comprador" });
     }
   }
 
-  
-  Passagems.init({
-    data_da_compra: DataTypes.DATE,
-    comprado: DataTypes.BOOLEAN,
-    cadeira: DataTypes.INTEGER,
-    comprador: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Passagems',
-  });
+  Passagems.init(
+    {
+      data_da_compra: DataTypes.DATE,
+      comprado: DataTypes.BOOLEAN,
+      cadeira: DataTypes.INTEGER,
+      comprador: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Passagems",
+    }
+  );
 
   return Passagems;
 };
